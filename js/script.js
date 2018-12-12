6f1d35f2b4ba218d1485e2fca63d0ff1425b8389
Justin Huey <juhuey1@s.sfusd.edu>
2018-12-12 01:58:58 +0000

    Layout

--- js/script.js
+++ js/script.js
@@ -2,16 +2,34 @@
 
 // BELOW Update the songs to your favorites songs. There should be at least 4.
 var songs = [
-    "24 Karat Magic",
-    "Nice For What",
-    "Universe"
+    "Africa",
+    "Never Gonna Give You Up",
+    "Ocean Man",
+    "We Are Number 1", 
+    "What Are You Doing In My Swamp",
+    "Bohemian Rhapsody",
+    "Sandstorm",
 ];
 // BELOW Add 4 More arrays to store images_links, atists, song lengths, and links for each song
 // Make sure they match the same order as your array above
 
+var images_links = ["https://www.youtube.com/watch?v=FTQbiNvZqaY",
+"https://www.youtube.com/watch?v=dQw4w9WgXcQ", 
+"https://www.youtube.com/watch?v=6E5m_XtCX3c",
+"https://www.youtube.com/watch?v=PfYnvDL0Qcw",
+"https://www.youtube.com/watch?v=VVggXZPCuYw&t=49s",
+"https://www.youtube.com/watch?v=jFKBR1ggTMY",
+"https://www.youtube.com/watch?v=gFsdPrBLHO0",]
 
+var artists = ["Toto", 
+"Rick Astley",
+"Ween",
+"Lazy Town",
+"Shrek",
+"Queen",
+"Darude"]
 
-
+var song_lengths = ["4:35", "3:32", "2:07", "2:50", "1:50", "6:02", "3:49"]
 
 
 function displaySongInfo(){
